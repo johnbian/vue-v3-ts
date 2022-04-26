3cc47dcc0e853def1b84dbb597ae0b78e6fa8ac3
johnbian <byh19920310@126.com>
2022-04-26 12:12:25 +0800

    feat: 升级cli

--- vue.config.js
+++ vue.config.js
@@ -1,38 +1,20 @@
 // vue.config.js
 const tsImportPluginFactory = require('ts-import-plugin');
 const proxy = require('./src/config/proxy.ts');
+const { defineConfig } = require('@vue/cli-service')
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      // 为生产环境...
-    } else {
-      // 为开发环境修改配置...
-    }
-    config.module.rules.push({
-      test: /\.(jsx|tsx|js|ts)$/,
-      use: [
-        {
-          loader: 'ts-loader',
-          options: {
-            happyPackMode: true, // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
-            transpileOnly: true,
-            getCustomTransformers: () => ({
-              before: [ tsImportPluginFactory({
-                libraryName: 'vant',
-                libraryDirectory: 'lib',
-                style: true
-              })]
-            }),
-            compilerOptions: {
-              module: 'es2015'
-            }
-          }
-        }
-      ],
-      exclude: /node_modules/
-    });
+
+  chainWebpack: config => {
+    // config.optimization.minimizer('terser')
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.optimization.minimizer('terser').tap(arg => {
+        arg[0].terserOptions.compress.drop_console = true
+        return arg
+      })
+    })
   },
 
   devServer: {
@@ -53,4 +35,4 @@ module.exports = {
   },
   
   parallel: false,
-}
+})
